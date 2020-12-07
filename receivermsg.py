45697bdc8f921247a5a598d703c76e7736425661
madaanp <73661923+madaanp@users.noreply.github.com>
2020-12-07 16:53:11 +0530

    Update receivermsg.py

--- receivermsg.py
+++ receivermsg.py
@@ -1,8 +1,8 @@
 import socket
 import time
 
-UDP_IP = "10.35.70.33"
-UDP_Port= 5005
+UDP_IP = "10.35.70.3"
+UDP_Port= 33001
 sock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(UDP_IP, UDP_PORT)
 
