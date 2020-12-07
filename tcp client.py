7cc7478b848b8c9ec5d9283ac4a67c1c258680c1
madaanp <73661923+madaanp@users.noreply.github.com>
2020-12-07 17:05:18 +0530

    tcp

---
+++ tcp client.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import socket
+TCP_IP = '10.35.70.3'
+TCP_PORT = 33001
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+print "received data:", data
